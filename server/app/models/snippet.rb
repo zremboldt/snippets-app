972cf896abc3e900abae8cebbfc9446e62a18a0c
zremboldt <zac.remboldt@joinroot.com>
2022-04-22 21:55:14 -0500

    RSpec tests

--- server/app/models/snippet.rb
+++ server/app/models/snippet.rb
@@ -1,4 +1,12 @@
 class Snippet < ApplicationRecord
   has_one_attached :image
   belongs_to :collection, optional: true
+
+  validate :validate_snippet
+
+  def validate_snippet
+    if title.blank? && note.blank? && !image.attached?
+        errors.add(:base, "title, note, or image must be present")
+    end
+  end
 end
--- server/spec/models/snippet_spec.rb
+++ server/spec/models/snippet_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
+# create comes from FactoryBot
+
 RSpec.describe Snippet, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe "When a snippet is created" do
-    let(:snippet) { create(:snippet, title: 'my title') }
+    let(:snippet) { create(:snippet, title: 'my title', note: 'my note') }
     
     it "should have a title" do
       expect(snippet.title).to eq("my title")
     end
-    # it "should have a note" do
-    #   snippet = Snippet.new
-    #   snippet.note = "Test Body"
-    #   expect(snippet.note).to eq("Test Body")
-    # end
+
+    it "should have a note" do
+      expect(snippet.note).to eq("my note")
+    end
   end
 
+  describe "When a snippet is created without a title, note, or image" do
+    it "should fail to create a snippet" do
+      snippet = Snippet.new
+      expect(snippet.save).to eq(false)
+    end
+  end
 end
