2593336d0a83db43152870c0f5d226d044c0271d
zremboldt <zac.remboldt@joinroot.com>
2022-06-08 23:29:16 -0500

    Created a job to analyze images with using the AWS Rekognition service

--- server/app/jobs/example_job.rb
+++ server/app/jobs/example_job.rb
@@ -2,6 +2,29 @@ class ExampleJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    # do all of the work with rekognition here
+
+    photo = 'app/assets/images/canyon.jpg'
+    path = File.expand_path(photo) # expand path relative to the current directory
+    file = File.read(path) # convert image to base64
+    
+    puts "----------------------------"
+    
+    client = Aws::Rekognition::Client.new
+    
+    response = client.detect_labels({
+      image: {
+        bytes: file,
+      }, 
+      max_labels: 10, 
+      min_confidence: 70, 
+    })
+    
+    # prints response as a hash
+    puts response.to_h
+    
+    puts "----------------------------"
+      
     # Do something later
     puts "hello world!"
   end
